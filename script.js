1e9a36713cff4f184ce3fbf228c316bb83fbf4ec
Paul-litsonmanok-Castro <paulcastro.0128@gmail.com>
2020-11-11 22:16:23 +0800

    sidebar complete

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navbarLinks = document.getElementsByClassName('nav-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+const arrowButton = document.getElementsByClassName('arrow-down-image')[0]
+const sidebar = document.getElementsByClassName('sidebar')[0]
+
+arrowButton.addEventListener('click', () => {
+    sidebar.classList.toggle('active')
+})
